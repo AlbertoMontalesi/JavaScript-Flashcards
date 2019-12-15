864b770676ec08bdbeeb46f9b2c3c014e8c53edb
albertomontalesi <alberto.montalesi@outlook.it>
2019-12-15 12:49:12 +0700

    Create repo with one file per array method

---
+++ script.js
@@ -0,0 +1,41 @@
+const array = [
+    "Array.of()",
+    "Array.prototype.concat()",
+    "Array.prototype.copyWithin()",
+    "Array.prototype.entries()",
+    "Array.prototype.every()",
+    "Array.prototype.fill()",
+    "Array.prototype.filter()",
+    "Array.prototype.find()",
+    "Array.prototype.findIndex()",
+    "Array.prototype.flat()",
+    "Array.prototype.flatMap()",
+    "Array.prototype.forEach()",
+    "Array.prototype.includes()",
+    "Array.prototype.indexOf()",
+    "Array.prototype.join()",
+    "Array.prototype.keys()",
+    "Array.prototype.lastIndexOf()",
+    "Array.prototype.map()",
+    "Array.prototype.pop()",
+    "Array.prototype.push()",
+    "Array.prototype.reduce()",
+    "Array.prototype.reduceRight()",
+    "Array.prototype.reverse()",
+    "Array.prototype.shift()",
+    "Array.prototype.slice()",
+    "Array.prototype.some()",
+    "Array.prototype.sort()",
+    "Array.prototype.splice()",
+    "Array.prototype.toLocaleString()",
+    "Array.prototype.toSource()",
+    "Array.prototype.toString()",
+    "Array.prototype.unshift()",
+    "Array.prototype.values()",
+]
+const fs = require('fs');
+
+
+for(const item of array){
+    fs.writeFileSync(`./Topics/Array Methods/${item}.md`);
+}
